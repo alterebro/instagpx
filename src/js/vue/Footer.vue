970589e27ac1228596565a21c11026b8e020c036
Jorge Moreno <alterebro@gmail.com>
2019-08-10 20:05:11 +0200

    Footer CSS Styling

--- src/js/vue/Footer.vue
+++ src/js/vue/Footer.vue
@@ -1,14 +1,16 @@
 <template>
     <footer role="contentinfo">
-        <p>A project created by <strong><a href="https://twitter.com/alterebro" target="_blank" rel="noopener noreferrer" title="Follow @alterebro on Twitter!">@alterebro</a></strong>.</p>
-        <nav>
-            <h4>Share! :</h4>
-            <ul>
-                <li v-for="network in networks" :class="network.network | networkClass">
-                    <a :href="network.url | networkURL" :title="network.network | networkTitle" @click="networkOpen($event)" :target="network.modal ? '_blank' : false" :rel="network.modal ? 'noopener noreferrer' : false">{{ network.network }}</a>
-                </li>
-            </ul>
-        </nav>
+        <div class="footer-content">
+            <p>A project created by <strong><a href="https://twitter.com/alterebro" target="_blank" rel="noopener noreferrer" title="Follow @alterebro on Twitter!">@alterebro</a></strong>.</p>
+            <nav>
+                <h4>Share! :</h4>
+                <ul>
+                    <li v-for="network in networks" :class="network.network | networkClass">
+                        <a :href="network.url | networkURL" :title="network.network | networkTitle" @click="networkOpen($event)" :target="network.modal ? '_blank' : false" :rel="network.modal ? 'noopener noreferrer' : false">{{ network.network }}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </footer>
 </template>
 
@@ -58,20 +60,33 @@ export default {
 @import "./../../scss/_variables.scss";
 
 footer[role="contentinfo"] {
-    
     background: #fff;
     border-top: solid #e6e6e6 1px;
-    padding: 2rem;
+}
 
+.footer-content {
+    width: 100%;
+    max-width: 92rem;
+    margin: 0 auto;
+    padding: 2rem;
     display: flex;
     align-items: center;
 
-    >p {
-        flex: 0 0 auto;
+    > p { flex: 0 0 auto }
+    > nav { flex: 1 1 auto }
+
+    @media #{$mobile} {
+        display: block;
+        > p {
+            text-align: center;
+            margin: 0 auto 1rem;
+        }
+        > nav {
+            margin: 0 auto;
+            justify-content: center;
+        }
     }
-    > nav {
-        flex: 1 1 auto;
-    }
+
 }
 
 nav {
@@ -82,65 +97,68 @@ nav {
     h4 { display: none }
     ul { display: flex }
     li {
+
+        // a {
+        //     border-bottom: solid transparent 2px;
+        //     transition: color .35s ease-in-out, border .35s ease-in-out;
+        //
+        //     &:hover {
+        //         color: #000;
+        //         border-bottom: solid $color-primary-light 2px;
+        //     }
+        // }
+        margin: 0 5px 0 0;
+        &:last-child {
+            margin: 0;
+        }
+
         a {
             display: block;
-            background: $color-primary-light;
-            color: #fff;
-            padding: 3px 10px 3px 26px;
-            margin: 0 3px;
-            border-radius: 0;
-            border: none;
-            position: relative;
-            transition: all .25s ease-in-out;
+            width: 28px;
+            height: 28px;
+            overflow: hidden;
+            border-bottom: none;
 
-            &:hover {
-                background: $color-primary-dark;
-                box-shadow: 0 5px 1px -2px rgba(0, 0, 0, .15);
-            }
 
             &:before {
                 content: '';
                 display: block;
-                width: 12px;
-                height: 12px;
-                position: absolute;
-                top: 50%;
-                left: 8px;
-                transform: translateY(-50%);
-                background-size: auto;
-                background-repeat: no-repeat;
+                width: 28px;
+                height: 28px;
+                border-radius: 3px;
+                transition: background .35s ease-in-out;
+                background-color: $color-primary-light;
+                background-size: auto 12px;
                 background-position: center center;
-                opacity: .5;
-                transition: opacity .25s ease-in-out;
-
-            }
-            &:hover:before {
-                opacity: 1;
             }
         }
 
-        &.twitter a:before { background-image: url(../../img/social-share/icon-share-twitter.svg); }
-        &.facebook a:before { background-image: url(../../img/social-share/icon-share-facebook.svg); }
-        &.linkedin a:before { background-image: url(../../img/social-share/icon-share-linkedin.svg); }
-        &.email a:before { background-image: url(../../img/social-share/icon-share-email.svg); }
-        &.telegram a:before { background-image: url(../../img/social-share/icon-share-telegram.svg); }
 
-        @media #{$tablet} {
-            a {
-                text-indent: -1000em;
-                padding: 0;
-                height: 32px;
-                width: 32px;
-                border-radius: 100%;
-                background: $color-primary-dark;
-
-                &:before {
-                    width: 12px;
-                    height: 12px;
-                    left: 10px;
-                }
-            }
+        &.twitter a:before {
+            // background-color: #1da1f2;
+            background-image: url(../../img/social-share/icon-share-twitter.svg);
         }
+        &.facebook a:before {
+            // background-color: #1877f2;
+            background-image: url(../../img/social-share/icon-share-facebook.svg);
+        }
+        &.linkedin a:before {
+            // background-color: #007bb5;
+            background-image: url(../../img/social-share/icon-share-linkedin.svg);
+        }
+        &.email a:before {
+            background-image: url(../../img/social-share/icon-share-email.svg);
+        }
+        &.telegram a:before {
+            // background-color: #0088cc;
+            background-image: url(../../img/social-share/icon-share-telegram.svg);
+        }
+
+        a:hover:before {
+            // background-color: $color-fg-dark;
+            background-color: $color-primary-dark;
+        }
+
     }
 }
 </style>
